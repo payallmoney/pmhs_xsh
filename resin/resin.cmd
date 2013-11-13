545041247494eff4c5783cdd99407ab92e183e24
payallmoney <payallmoney@163.com>
2013-11-13 17:12:18 +0800

    增加了resin的配置文件

--- resin/resin.cmd
+++ resin/resin.cmd
@@ -1,6 +1,6 @@
-@d:
-@cd D:\pmhs\pmhs_xsh\resin
-@set RESIN_HOME=D:\resin-4.0.32
-@%RESIN_HOME%\resin.exe console  -conf %cd%\conf\resin.xml  -Xms512m -Xmx512m -XX:MaxNewSize=128m -XX:MaxPermSize=128m
+d:
+cd D:\workspace\pmhs_sd\resin
+set RESIN_HOME=D:\resin-4.0.32
+%RESIN_HOME%\resin.exe console  -conf %cd%\conf\resin.xml  -Xms512m -Xmx512m -XX:MaxNewSize=128m -XX:MaxPermSize=128m
 
 cmd /k
