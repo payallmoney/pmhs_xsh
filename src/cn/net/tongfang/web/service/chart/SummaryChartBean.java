5ccdf61a8b1a945cd33ff20b7274647a4f93d1c3
zhengxiong <17314156@qq.com>
2014-01-04 17:12:45 +0800

    图标展现和百度地图更新

---
+++ src/cn/net/tongfang/web/service/chart/SummaryChartBean.java
@@ -0,0 +1,24 @@
+package cn.net.tongfang.web.service.chart;
+
+public class SummaryChartBean {
+
+	private String xmlData;
+	private String selectData;
+
+	public String getXmlData() {
+		return xmlData;
+	}
+
+	public void setXmlData(String xmlData) {
+		this.xmlData = xmlData;
+	}
+
+	public String getSelectData() {
+		return selectData;
+	}
+
+	public void setSelectData(String selectData) {
+		this.selectData = selectData;
+	}
+
+}
