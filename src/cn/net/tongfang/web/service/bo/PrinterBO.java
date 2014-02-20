4c24b1b30d6ce70d1c670618a20053a39a3eb7bc
xsh <xsh@gmail.com>
2014-02-20 19:55:46 +0800

    居民健康档案与健康体检记录打印程序

---
+++ src/cn/net/tongfang/web/service/bo/PrinterBO.java
@@ -0,0 +1,19 @@
+package cn.net.tongfang.web.service.bo;
+
+public class PrinterBO {
+	private String printType;
+	private String printWhere;
+	public String getPrintType() {
+		return printType;
+	}
+	public void setPrintType(String printType) {
+		this.printType = printType;
+	}
+	public String getPrintWhere() {
+		return printWhere;
+	}
+	public void setPrintWhere(String printWhere) {
+		this.printWhere = printWhere;
+	}
+	
+}
