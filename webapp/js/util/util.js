471639e11040e93533f542109642c71bf46e5bf1
payallmoney <payallmoney@163.com>
2014-03-11 15:25:18 +0800

    1,修改了婚检的加解密

--- webapp/js/util/util.js
+++ webapp/js/util/util.js
@@ -24,24 +24,25 @@ function denc(str){
 
 
 function enc(str){
-    if(! str || !str.length){
-        return "";
-    }
-    var denclist = '$&@*!.:=>}€‚ƒˆ‰Š‹ŒŽ‘’•–àáâãäæççèéêëìßÞÝÜÛÜÛÚÙØÖÕÔÓÒÑÐÏÊÉÇÆÄÃ£Á';
-    var enclist =  '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    var result = "";
-    var tmpStr = "";
-    for (var i = 0; i < str.length; i++) {
-        tmpStr = str.substr(i,1);
-        if (tmpStr !== "%") {
-            var index = enclist.indexOf(tmpStr);
-            if (index<0) {
-                tmpStr = String.fromCharCode(tmpStr.charCodeAt(0) ^ 'c'.charCodeAt(0));
-            } else {
-                tmpStr = denclist.substr(index,1);
-            }
-        }
-        result = result + tmpStr;
-    }
-    return result;
+	return str;
+//    if(! str || !str.length){
+//        return "";
+//    }
+//    var denclist = '$&@*!.:=>}€‚ƒˆ‰Š‹ŒŽ‘’•–àáâãäæççèéêëìßÞÝÜÛÜÛÚÙØÖÕÔÓÒÑÐÏÊÉÇÆÄÃ£Á';
+//    var enclist =  '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+//    var result = "";
+//    var tmpStr = "";
+//    for (var i = 0; i < str.length; i++) {
+//        tmpStr = str.substr(i,1);
+//        if (tmpStr !== "%") {
+//            var index = enclist.indexOf(tmpStr);
+//            if (index<0) {
+//                tmpStr = String.fromCharCode(tmpStr.charCodeAt(0) ^ 'c'.charCodeAt(0));
+//            } else {
+//                tmpStr = denclist.substr(index,1);
+//            }
+//        }
+//        result = result + tmpStr;
+//    }
+//    return result;
 }
