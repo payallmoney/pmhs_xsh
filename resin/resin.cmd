28f159e3f7cda4fafb4ba6658ede382c99e755c5
payallmoney <payallmoney@163.com>
2015-04-12 17:36:42 +0800

    1,删除resin目录

--- resin/resin.cmd
+++
@@ -1,6 +0,0 @@
-@d:
-@cd D:\workspace\pmhs_km\resin
-@set RESIN_HOME=D:\resin-4.0.32
-@%RESIN_HOME%\resin.exe console  -conf %cd%\conf\resin.xml  -Xms512m -Xmx512m -XX:MaxNewSize=128m -XX:MaxPermSize=128m
-
-cmd /k
